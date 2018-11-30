756a327c10acaa232dd475927c87d9175cce0be9
CornelKuiper <cornelk@live.nl>
2018-11-30 12:04:57 +0100

    Add files via upload

---
+++ rosen.py
@@ -0,0 +1,18 @@
+import numpy as np 
+
+
+class Network(object):
+    def __init__(self):
+
+
+
+
+
+def main():
+	if not len(sys.argv)==5:
+        print("Incorrect amount of arguments, required: 4")
+        sys.exit(-1)
+    filename = sys.argv[1]
+
+if __name__ == "__main__":
+	main()
