14315726ce8a9faf041d0356ebc24b4657f59ea7
asd@ASdasd.asd <asd@ASdasd.asd>
2018-12-10 14:04:41 +0100

    final changes, hopefully

--- rosen.py
+++ rosen.py
@@ -8,15 +8,28 @@ from tqdm import trange, tqdm
 class Network(object):
     def __init__(self, N):
         # self.w = np.random.randn(N) / 2
-        self.w = np.zeros([N, 1])
+        self.w = np.zeros([N,1])
         # self.b = np.random.randn(1) / 2
-        self.b = 0.0
+        self.b = -1.0
         self.c = 0.5
         self.learning_rate = 0.1
         self.error = []
 
-    def r_perceptron(self):
-        pass
+    def train_old(self, features, labels, epochs, N):
+        for epoch in trange(epochs):
+            for x, y in zip(features, labels):
+                E = ((np.dot(x, self.w) + self.b) * y)[0]
+                E_ = (E < self.c).astype(float)
+                self.w += (1.0 / N) * E_ * x * y
+                loss += E_
+
+            self.error.append(loss)
+            if loss <= 0:
+                break        
+        # plt.plot(self.error)
+        # plt.ylabel('error')
+        # plt.show()
+        return loss <= 0
 
     def train(self, features, labels, epochs, N):
         for epoch in trange(epochs):
@@ -24,13 +37,10 @@ class Network(object):
             E = ((np.dot(features, self.w) + self.b) * labels)
             E_ = (E < self.c).astype(float)
             self.w += np.expand_dims(np.mean(E_ * features * labels, axis=0), -1)
+            self.b += np.mean(E_ * features * labels)
             loss += np.mean(E_)
 
-            # for x, y in zip(features, labels):
-            #     E = ((np.dot(x, self.w) + self.b) * y)[0]
-            #     E_ = (E < self.c).astype(float)
-            #     self.w += (1.0 / N) * E_ * x * y
-            #     loss += E_
+
             self.error.append(loss)
             if loss <= 0:
                 break        
@@ -43,7 +53,7 @@ class Network(object):
 
 def main():
     if not len(sys.argv)>=3:
-        print("Incorrect amount of arguments, required: 2, optional: epochs")
+        print("Incorrect amount of arguments, required: 2, optional: epochs, nd")
         print("Give P, N and epochs")
         sys.exit(-1)
     # the amount of random vectors
@@ -55,6 +65,11 @@ def main():
         epochs = int(sys.argv[3])
     else:
         epochs = 100 
+    if len(sys.argv)>4:
+        nd = int(sys.argv[4])
+    else:
+        nd = 10
+
 
     # generate features of dimension N with random floats sampled from a univariate normal (Gaussian) distribution of mean 0 and variance 1
     features = np.random.randn(P,N)
@@ -66,12 +81,12 @@ def main():
 
     # print(f"Features shape: {features.shape}")
     # print(f"Labels shape: {labels.shape}")
-    alphas = np.arange(0.75, 3  , 0.1)
+    alphas = np.arange(0.75, 3, 0.1)
     runs = np.zeros_like(alphas)
     for idx_alpha, alpha in enumerate(tqdm(alphas)):
         success = []
         for idx_n, N in enumerate(tqdm([20, 40, 60, 80, 100])):
-            for run in trange(10):
+            for run in trange(nd):
                 P = int(alpha*N)
                 features = np.random.randn(P,N)
                 # make random labels with 50/50 chance for a -1 or 1
@@ -83,7 +98,8 @@ def main():
     plt.plot(alphas, runs)
     plt.ylabel('success rate')
     plt.xlabel('alpha')
-    plt.show()
+    # plt.show()
+    plt.savefig('result.png')
 
 if __name__ == "__main__":
     main()
