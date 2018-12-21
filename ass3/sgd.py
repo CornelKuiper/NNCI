19bcf98946a2f0f66371617251540714af5465f5
asd@ASdasd.asd <asd@ASdasd.asd>
2018-12-21 16:53:37 +0100

    asdf;jdasfkdjas

--- ass3/sgd.py
+++ ass3/sgd.py
@@ -11,6 +11,8 @@ class Network(object):
         self.w /= np.sum(self.w**2, 0, keepdims=True)
         self.lr = 0.05
 
+    def der_tanh(self, x):
+        return 1 - x ** 2
 
     def sgd_(self, x, y):
         y_ = np.tanh(np.dot(x, self.w))
@@ -19,29 +21,29 @@ class Network(object):
         e = 1
         self.w -= 1
 
-    def sgd(self, x, y, steps=100, epochs=100):
+    def sgd(self, x, y, steps=5000, epochs=100):
         P = x.shape[0]  * 1.0
         loss = []
-        # for x_, y_ in zip(x, y):
-        for epoch in range(epochs):
+        for epoch in trange(epochs):
             idx_ = np.random.randint(P, size=steps)
             for idx in idx_:
                 x_, y_ = x[idx], y[idx]
-                sig = np.tanh(np.dot(x_, self.w))
-                sig = np.sum(sig)
-                e_v = 0.5 * np.square(sig - y_)
-                loss.append((1 / P) * e_v)
-                sig = np.tanh(np.dot(x_, self.w))
-                gradient0 = ((sig[0] - y_) * (1 - sig[0] ** 2) * x_).reshape(50,1)
-                gradient1 = ((sig[1] - y_) * (1 - sig[1] ** 2) * x_).reshape(50,1)
+                dot = np.dot(x_, self.w)
+                sig = np.tanh(dot)
+                E = sig - y_
+                output = np.sum(sig)
+                e_v = 0.5 * np.square(output - y_)
+                # print(dot)
+                # print((E[0] * self.der_tanh(dot[0]) * dot).shape)
+                gradient0 = (E[0] * self.der_tanh(dot[0]) * x_) .reshape(50,1)
+                gradient1 = (E[1] * self.der_tanh(dot[1]) * x_).reshape(50,1)
                 gradient = np.concatenate([gradient0,gradient1],axis=1)
-                print(gradient.shape)
-                self.w -= gradient
+                self.w -= self.lr * gradient * e_v
 
-            # sig = np.tanh(np.dot(x, self.w))
-            # sig = np.sum(sig)
-            # e_v = np.mean(0.5 * np.square(sig - y))
-            # loss.append((1 / P) * e_v)
+            sig = np.tanh(np.dot(x, self.w))
+            sig = np.sum(sig,axis=1).reshape(5000,1)
+            e_v = np.mean(0.5 * np.square(sig - y))
+            loss.append(e_v)
 
         plt.plot(loss)
         plt.show()
